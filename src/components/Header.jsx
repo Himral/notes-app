aecd62758c53442affcdadbda5079018c07a3e4d
Himral <himralgarg@gmail.com>
2024-03-10 12:43:22 +0530

    updates
    Signed-off-by: Himral <himralgarg@gmail.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header()
 {
     return(
         <div className='header'>
-            <img className = 'trollface' src = "notes.png"></img>
+            <img className = 'trollface' src = "notes_main.png"></img>
             <h4>Notes App</h4>
             <h4>Check this out!</h4>
         </div>
